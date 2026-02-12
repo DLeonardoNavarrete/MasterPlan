93a71ccccb71da7f2f82ebcbf2d9ddf1aa4afccc
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-02-12 17:21:07 -0500

    Componets-SocialMedia

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+const SocialMedia = ({ className }) => {
+    const redes =[
+        {
+            nombre:'Instagram',
+            icon:'fa-brands fa-instagram',
+            url:'https://www.instagram.com/masterplan.proyectos/',
+            color:'#E4405F'
+        },
+        {
+            nombre:'Whatsapp',
+            icon:'fa-brands fa-whatsapp',
+            url:'https://wa.me/573208233592?text=Hola%20M_P%20estoy%20interesado%20en%20su%20interventoría',
+            color:'#25D366'
+        },
+    ];
+    return (
+        <div className={`social-container${className}`}>
+            {redes.map((red)=>(
+                <a
+                key={red.nombre}
+                href={red.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-button"
+                >
+                    <i className={red.icon}></i>
+                </a>
+            ))}
+        </div>
+    );
+};
+export default SocialMedia;
