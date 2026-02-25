6bd5aa1d6c7f55cf16713cfd1cc2bbc72015d1a9
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-02-25 14:19:24 -0500

    ¡conection!Supabase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,7 @@ export default function Login() {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        const { error } = await supabase.auth.signInWithPassword({ 
+        const { error, data } = await supabase.auth.signInWithPassword({ 
             email: email,
             password: password,
         });
