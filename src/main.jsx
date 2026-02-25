381a9eb7c5afdafb36e563e97ae996d55a4e7ecc
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-02-25 14:33:44 -0500

    Analizador_de_página.none

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css?=v1.1'
 import './App.css?=v1.1'
 import App from './App.jsx'
-import { Analytics } from '@vercel/analytics/next'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
