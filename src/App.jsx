2ff3466a285b08121484cdb052dc2706185dc61d
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-02-25 16:08:53 -0500

    interfaz_login

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import DetalleImagen from './components/DetalleImagen';
 import DetalleProyecto from './components/DetalleProyecto';
 import { useState, useEffect } from 'react';
 import SkeletonCarrusel from './components/SkeletonCarrusel';
-import Login from './componenets/Login';
+import Login from './components/Login';
 
 const Home = () => {
 const [loading, setLoading] = useState(true);
