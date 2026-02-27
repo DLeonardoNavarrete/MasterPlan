7752a93406c19bfc802ecf0968b152be09fdb4ac
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-02-27 12:16:15 -0500

    datos_usuario

--- src/components/VistaUsuario.jsx
+++ src/components/VistaUsuario.jsx
@@ -3,18 +3,12 @@ export default function Userview({ info }) {
 
   return (
     <div className="user-card">
-      <header className="card-header">
-        <h3>📄 Ficha de Empleado</h3>
+        <h2>📄 Ficha de {info.nombre_usuario}</h2>
         <span className="status-badge">Activo</span>
-      </header>
-      
       <div className="card-content">
         <div className="info-group">
           <label>Nombre Completo</label>
           <p>{info.nombre_usuario} {info.apellido_usuario}</p>
-        </div>
-
-        <div className="info-grid">
           <div>
             <label>Cédula</label>
             <p>{info.cedula_usuario}</p>
@@ -31,9 +25,6 @@ export default function Userview({ info }) {
             <label>Tipo de Contrato</label>
             <p>{info.tipo_contrato}</p>
           </div>
-        </div>
-
-        <div className="info-footer">
           <label>Fecha de Contratación:</label>
           <span>{info.fecha_contrato}</span>
         </div>
