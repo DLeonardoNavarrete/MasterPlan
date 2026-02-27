f59e820dc433b8a09aa8841245aec6da9a19f1d4
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-02-27 08:56:17 -0500

    interfaz-perfil

---
+++ src/components/AuthStatus.jsx
@@ -0,0 +1,21 @@
+import { useNavigate, Link } from 'react-router-dom';
+import { supabase } from '../supabaseClient';
+
+const AuthStatus = ({ seccion }) => {
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        await supabase.auth.signOut();
+        navigate('/');
+    };
+    if (seccion) {
+        return(
+            <div className="user-info">
+                <span> {seccion.user.email}</span>
+                <Link to="/admin" className="btn-perfil">Perfil</Link>
+                <button onClick={handleLogout} className="btn-salir-sesion">Cerrar sesión</button>
+            </div>
+        );
+    }
+    return <Link to="/login" className="btn-iniciar-sesion">Iniciar sesión</Link>;
+}
+export default AuthStatus;
