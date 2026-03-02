b160f4182141f9c720a2e60a942df767e2a7475e
Leonardo <davidleonardonavarrete.c@gmail.com>
2026-03-02 11:59:06 -0500

    Re-Estructuración_Proyecto.v2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../../api/supabaseClient';
+import { supabase } from '../supabaseClient';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login({ onBack }) {
